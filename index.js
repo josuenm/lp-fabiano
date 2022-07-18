a1a46a32a3950153c15b475be6dc65e393d0b477
josuenm <direct.josue@gmail.com>
2022-07-18 14:07:22 -0300

    Pegar localização [REMOVIDO]

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-async function api() {
-  await fetch('https://ssl.geoplugin.net/json.gp?k=292af9fb00c1353d')
-    .then((res) => res.json())
-    .then(
-      (data) =>
-        (document.querySelector('header .city').innerHTML = data.geoplugin_city)
-    );
-}
-api();
-
 let listTitle = document
   .querySelector('.selected-title')
   .innerText.toLowerCase()
